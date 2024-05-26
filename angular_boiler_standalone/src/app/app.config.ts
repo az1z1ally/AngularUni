1c3d10ab6acfd7ef9729cb128e3362be3c079fc8
az1z1ally <albyally5@gmail.com>
2024-05-26 15:41:36 +0300

    Upgraded to angular 18

--- angular_boiler_standalone/src/app/app.config.ts
+++ angular_boiler_standalone/src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { ToastrConfig } from './toastr.config';
 import { tokenInterceptor } from './shared/interceptors/token.interceptor';
-import { HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { provideToastr } from 'ngx-toastr';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -15,13 +15,18 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideClientHydration(), 
-    provideHttpClient(withFetch()),
-    importProvidersFrom(HttpClientModule, ReactiveFormsModule),
-    { 
-      provide: HTTP_INTERCEPTORS, 
-      useValue: tokenInterceptor, 
-      multi: true // Set to true to allow multiple interceptors
-    },
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([
+        tokenInterceptor
+      ])
+    ),
+    importProvidersFrom(ReactiveFormsModule),
+    // { 
+    //   provide: HTTP_INTERCEPTORS, 
+    //   useValue: tokenInterceptor, 
+    //   multi: true // Set to true to allow multiple interceptors
+    // },
     provideAnimations(), // required animations providers
     provideToastr(ToastrConfig), // Toastr providers
     provideAnimationsAsync(), provideAnimationsAsync()
